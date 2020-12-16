a4c37d6685ff38915bab0f340938efc62a1866ac
JongilSohn <sji7532@gmail.com>
2020-12-16 18:49:48 +0900

    fNext api 설정

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,34 +1,75 @@
-import Document from 'next/document'
+// import Document from 'next/document'
+// import { ServerStyleSheet } from 'styled-components'
+
+// export default class MyDocument extends Document {
+//   static async getInitialProps(ctx) {
+//     // 1.  ServerStyleSheet의 인스턴스 생성
+//     const sheet = new ServerStyleSheet()
+//     const originalRenderPage = ctx.renderPage
+
+//     try {
+//       // 하기 코드는 Next js에서 제공하는 샘플 코드입니다.
+
+
+//       ctx.renderPage = () =>
+//         originalRenderPage({
+//           enhanceApp: (App) => (props) =>
+//             sheet.collectStyles(<App {...props} />),
+//         })
+
+//       // Documents의 initial props
+//       const initialProps = await Document.getInitialProps(ctx)
+
+//       // props와 styles를 반환
+//       return {
+//         ...initialProps,
+//         styles: (
+//           <>
+//             {initialProps.styles}
+//             {sheet.getStyleElement()}
+//           </>
+//         ),
+//       }
+//     } finally {
+//       sheet.seal()
+//     }
+//   }
+// }
+
+
+import Document, { Head, Main, NextScript } from 'next/document';
+import React from 'react';
 import { ServerStyleSheet } from 'styled-components'
 
-export default class MyDocument extends Document {
-  static async getInitialProps(ctx) {
-    const sheet = new ServerStyleSheet()
-    const originalRenderPage = ctx.renderPage
+// import { ServerStyleSheet } from '../styles/themed-components';
 
-    try {
-      // sheet을 사용해 정의된 모든 스타일을 수집
-      ctx.renderPage = () =>
-        originalRenderPage({
-          enhanceApp: (App) => (props) =>
-            sheet.collectStyles(<App {...props} />),
-        })
+interface StyledProps {
+  styleTags: Array<React.ReactElement<{}>>;
+}
 
-      // Documents의 initial props
-      const initialProps = await Document.getInitialProps(ctx)
+export default class MyDocument extends Document<StyledProps> {
+  static getInitialProps({ renderPage }) {
+    const sheet = new ServerStyleSheet();
+    const page = renderPage((App) => (props) =>
+      sheet.collectStyles(<App {...props} />),
+    );
 
-      // props와 styles를 반환
-      return {
-        ...initialProps,
-        styles: (
-          <>
-            {initialProps.styles}
-            {sheet.getStyleElement()}
-          </>
-        ),
-      }
-    } finally {
-      sheet.seal()
-    }
+    const styleTags = sheet.getStyleElement();
+    return { ...page, styleTags };
+  }
+
+  render() {
+    return (
+      <html>
+        <Head>
+          <title>Sohns</title>
+          {this.props.styleTags}
+        </Head>
+        <body>
+          <Main/>
+          <NextScript />
+        </body>
+      </html>
+    );
   }
 }
